f852af4f37aa6b511b5309b19804b3a98f2221ab
SameplayerDE <asameplayer@gmail.com>
2023-12-18 20:57:11 +0100

    changes

---
+++ QColonFrame/Input/QCInputAction.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QColonFrame.Input
+{
+    public class QCInputAction
+    {
+
+        public enum TriggerState
+        {
+            Pressed,
+            Released,
+            Down,
+            Up
+        }
+
+        public Keys? Key;
+        public TriggerState State;
+        public bool IsTriggered;
+
+    }
+}
