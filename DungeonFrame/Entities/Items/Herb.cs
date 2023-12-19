02410d282cf4c65ac4525edc69df2175d58992eb
SameplayerDE <asameplayer@gmail.com>
2023-12-19 13:12:33 +0100

    stuff

---
+++ DungeonFrame/Entities/Items/Herb.cs
@@ -0,0 +1,13 @@
+﻿using QColonFrame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonFrame.Entities.Items
+{
+    public class Herb : QCEntity
+    {
+    }
+}
--- DungeonFrame/QuestSystem/QuestObjectiv.cs
+++ DungeonFrame/QuestSystem/QuestObjectiv.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QColonFrame;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,5 +19,45 @@ namespace DungeonFrame.QuestSystem
         }
 
         public abstract void UpdateProgress();
+    }
+
+    public class KillObjective : QuestObjective
+    {
+        public int RequiredAmount { get; private set; }
+        public int CurrentAmount { get; private set; }
+
+        public KillObjective(string description, int requiredAmount) : base(description)
+        {
+            RequiredAmount = requiredAmount;
+            CurrentAmount = 0;
+        }
+
+        public override void UpdateProgress()
+        {
+            if (CurrentAmount >= RequiredAmount)
+            {
+                IsCompleted = true;
+            }
+        }
+    }
+
+    public class CollectObjective<T> : QuestObjective where T : QCEntity 
+    {
+        public int RequiredAmount { get; private set; }
+        public int CurrentAmount { get; private set; }
+
+        public CollectObjective(string description, int requiredAmount) : base(description)
+        {
+            RequiredAmount = requiredAmount;
+            CurrentAmount = 0;
+        }
+
+        public override void UpdateProgress()
+        {
+            if (CurrentAmount >= RequiredAmount)
+            {
+                IsCompleted = true;
+            }
+        }
     }
 }
